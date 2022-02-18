2d1a9c760baf59afc4c960b0c7af3d7fae939277
foammy <me@foammy.pw>
2022-02-18 21:09:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+﻿<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>ебет?</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/fonts.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://kit.fontawesome.com/3deb558b87.js" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<script src="assets/js/title.js"></script>
+	<meta name="theme-color" content="#0E0F18">
+	<link href="assets/images/eblan.png" rel="shortcut icon">
+
+</head>
+<body>
+	<div class="preloader">
+		<div class="preloader__row">
+		  	<div class="preloader__item"></div>
+		  	<div class="preloader__item"></div>
+		</div>
+	</div>
+
+
+	<script>
+		window.onload = function () {
+			document.body.classList.add('loaded');
+		}
+	</script>
+	<script>
+		window.onload = function () {
+		document.body.classList.add('loaded_hiding');
+		window.setTimeout(function () {
+			document.body.classList.add('loaded');
+			document.body.classList.remove('loaded_hiding');
+		}, 500);
+		}
+	</script>
+	<script> document.oncontextmenu = cmenu; function cmenu() { return false; } </script>
+
+	<section class="hero col-md-12">
+		<div class="hero-inner">
+			<h1> foammy </h1>
+			<span class="typed">фоми. за дос сват напоминаю. 17 y.o.</span>
+			<br>
+			<br>
+			<audio src="assets/mp3/play.mp3" loop autoplay preload="auto">
+			<script>
+				document.currentScript.parentElement.volume = 0.2;
+			</script>
+			</audio>
+			<br>
+			<br>
+			<a href="https://vk.com/foammy" target="_blank" onclick="window.open('https://vk.com/foammy);"><img alt="VK" class="social-media" height="32" src="assets/images/vk.svg" width="32">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a href="tg://resolve?domain=xakin_ovh" target="_blank"><img alt="TG" class="social-media" height="32" src="assets/images/telegram.svg" width="32">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a href="github.html" onclick="github.html"><img alt="GIT" class="social-media" height="32" src="assets/images/github.svg" width="32">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a href="https://blog.foammy.pw" onclick="https://blog.foammy.pw"><img alt="BLOG" class="social-media" height="32" src="assets/images/donate.svg" width="32">
+			<br>
+		  </div>
+	</section>	
+</body>
+</html>
+
